d8e7df7f2d69da332dfe4a09923c85d22640056d
Deep <ranpariyadeepchandubhai@gmail.com>
2025-05-15 09:52:03 +0200

    Verification & Error Habdling

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const ejs = require("ejs");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 // URL for connect to MongoDB
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -37,11 +40,10 @@ app.use(methodOverride("_method"));
 // Middleware to use ejs-mate for layout
 app.engine("ejs", ejsMate);
 
-
 // root API
-app.get("/", (req, res) => {
-  res.send("I am a Root!");
-});
+// app.get("/", (req, res) => {
+//   res.send("I am a Root!");
+// });
 
 // API to render Listinga (Index Route)
 app.get("/listings", async (req, res) => {
@@ -49,49 +51,75 @@ app.get("/listings", async (req, res) => {
   res.render("./listings/index.ejs", { allListings });
 });
 
-
 //New Route (form to create a new listing)
 app.get("/listings/new", (req, res) => {
-    res.render("./listings/new.ejs");
-  });
+  res.render("./listings/new.ejs");
+});
 
-  //Create Route (create a new listing)
-app.post("/listings", async (req, res) => {
-  //  let { title, description, image, price, location, country } = req.body;
-    let newListing = new Listing(req.body.listing);
+const validateListing = (req, res, next) => {
+  let {error} =  listingSchema.validate(req.body);
+    if (error) {
+      let errMsg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(400, errMsg);
+    }else{
+      next();
+    }
+};
+
+//Create Route (create a new listing)
+app.post(
+  "/listings",validateListing,
+  wrapAsync(async (req, res, next) => {
+    //  let { title, description, image, price, location, country } = req.body;
+   
+
+    const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-   
-});
+  })
+);
 
 //Show Route ( full details of a specific listing)
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("./listings/show.ejs", { listing });
-});
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/show.ejs", { listing });
+  })
+);
 
 //Edit Route (form to edit a listing)
-app.get("/listings/:id/edit", async (req,res) =>{
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
     let { id } = req.params;
-const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id);
     res.render("./listings/edit.ejs", { listing });
-});
+  })
+);
 
 //Update Route (update a listing)
 
-app.put("/listings/:id", async (req, res) => {
+app.put(
+  "/listings/:id",validateListing,
+  wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing});
-    res.redirect(`/listings/${id}`);
+   await Listing.findByIdAndUpdate(id, {
+      ...req.body.listing,
     });
+    res.redirect(`/listings/${id}`);
+  })
+);
 
-    //Delete Route (delete a listing)
-app.delete("/listings/:id", async (req, res) => {
+//Delete Route (delete a listing)
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-}
+  })
 );
 
 //Test Listing API
@@ -108,6 +136,14 @@ app.delete("/listings/:id", async (req, res) => {
 //   console.log("Sample listing saved");
 //     res.send("success");
 // });
+
+//Error handling middleware
+//pela badha upar na route ma check kare no male to baaki badh aroutni req. ma page not found
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Somthing went wrong!" } = err;
+  res.status(statusCode).render("error.ejs",{message});
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
---
+++ schema.js
@@ -0,0 +1,12 @@
+const Joi = require('joi');
+
+module.exports.listingSchema = Joi.object({
+    listing: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        images: Joi.string().allow("",null),
+        country: Joi.string().required(),
+    }).required()
+});
