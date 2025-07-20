c25a8e9d8fa0fba0f3c60007cbe7abe717606974
Deep <ranpariyadeepchandubhai@gmail.com>
2025-07-21 00:30:44 +0200

    Review Schema Created

---
+++ models/review.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+// Define the schema for the review
+// we save the review in the database
+// Comments, rating and time of creation
+const reviewSchema = new Schema({
+    comment:String,
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true,
+    },
+        createdAt: {
+        type: Date,
+        default: Date.now(),
+    }
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
+
